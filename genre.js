d665d03f749cd257441a5510fec61d4a5c230eed
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2023-03-22 00:09:06 +0800

    Initial commit

---
+++ genre.js
@@ -0,0 +1,20 @@
+const cheerio = require('cheerio')
+const axios = require('axios')
+
+const url = "http://books.toscrape.com/catalogue/category/books/mystery_3/index.html"
+
+async function getGenre() {
+  try {
+    const response = await axios.get(url)
+    const $ = cheerio.load(response.data)
+
+    const genre = $("h1").text();
+
+    console.log(genre)
+
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+getGenre()
